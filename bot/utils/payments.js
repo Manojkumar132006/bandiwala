e31c839799567c3b45fd8bb0f3c309625dc6b31d
Manojkumar132006 <63360464+Manojkumar132006@users.noreply.github.com>
2025-03-13 21:14:59 +0530

    Update payments.js

--- bot/utils/payments.js
+++ bot/utils/payments.js
@@ -4,6 +4,28 @@ const rzp = new Razorpay({
     key_secret: process.env.RAZORPAY_KEY_SECRET
 })
 
-export async function createOrder(){
-
+export function createOrder(amount,customer,items){
+    let Order= null
+    rzp.paymentLink.create({
+        amount: amount,
+        currency: 'INR',
+        accept_partial: false,
+        customer:{
+            name: customer.name,
+            email: customer.email,
+            contact: customer.phone
+        },
+        notify:{
+            sms: true
+        },
+        notes:{
+            description:`${items}`
+        },
+        remainder_enable: true,
+        callback_url:'',
+        callback_method:''
+    },(err,order)=>{
+        Order =JSON.parse(order);
+    })
+    return Order
 }
